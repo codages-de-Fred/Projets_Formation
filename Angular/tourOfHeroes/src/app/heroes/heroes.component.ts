3241be53eb0c2fd72d4a81ed0b18ba67eddd7886
fred80 <fred.nath.lui@live.fr>
2020-07-27 15:14:19 +0200

    liste des héroes + séparer hero et heroes

--- Angular/tourOfHeroes/src/app/heroes/heroes.component.ts
+++ Angular/tourOfHeroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 //on import Hero qu'on a créé
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -13,12 +14,21 @@ export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
     name: 'Patman'
-  };
+  }
+  //on récupère le tableau HEROES de mock-heroes.ts
+  heroes = HEROES;
+  //pour afficher le hero sélectionné
+  selectedHero: Hero;
 
   constructor() { }
   //lors de l'initialisation de la page
   //ngAfterViewInit() est équivalent de DOMLoadedContent
   ngOnInit(): void {
+  }
+  //déclare une fct permettant de sélectionner un héro
+  //cette fct est appelée ds app.component.html
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
