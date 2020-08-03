4af92c3fbc795ee4096040075ce6cdae86bcf7a1
fred80 <fred.nath.lui@live.fr>
2020-08-03 11:27:16 +0200

    pour mettre au format français

--- Angular/angularWeather/src/app/app.module.ts
+++ Angular/angularWeather/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +7,10 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { WeatherComponent } from './weather/weather.component';
 import { SearchNominationComponent } from './search-nomination/search-nomination.component';
+
+import fr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(fr);
 
 @NgModule({
   declarations: [
@@ -19,7 +23,10 @@ import { SearchNominationComponent } from './search-nomination/search-nomination
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    //permet de définir la locale par défaut en français
+    { provide: LOCALE_ID, useValue: 'fr-FR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
