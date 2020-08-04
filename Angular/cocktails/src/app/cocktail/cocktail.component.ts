4f9e1a69da5a20aa8fe3b44bdb26381271bd0044
fred80 <fred.nath.lui@live.fr>
2020-08-04 16:24:31 +0200

    bouton de retour à l'affichage précédent

--- Angular/cocktails/src/app/cocktail/cocktail.component.ts
+++ Angular/cocktails/src/app/cocktail/cocktail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CocktailService } from "../service/cocktail.service";
 import { ActivatedRoute } from "@angular/router";
+//location permet d'enregistrer l'adresse précédente
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-cocktail',
@@ -9,7 +11,7 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class CocktailComponent implements OnInit {
 
-  constructor(private cocktail: CocktailService, private route: ActivatedRoute) { }
+  constructor(private cocktail: CocktailService, private route: ActivatedRoute, private location: Location) { }
 
   name: string;
   thisCocktail: object
@@ -24,6 +26,10 @@ export class CocktailComponent implements OnInit {
   getCocktail(name: string) {
     this.cocktail.getCocktail(name).subscribe(data => console.log(data))
     return this.cocktail.getCocktail(name).subscribe(data => this.thisCocktail = (data))
+  }
+  //retour à l'affichage précédent
+  goback() {
+    this.location.back();
   }
 
 }
