62423fc35b6292b8986f0e0a39ecb8a5956a3246
fred80 <fred.nath.lui@live.fr>
2020-08-25 14:42:28 +0200

    affichage du formulaire create + liste des auteurs

--- Angular/book-app/src/app/authors/all-authors/all-authors.component.ts
+++ Angular/book-app/src/app/authors/all-authors/all-authors.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AllServicesService } from "../services/all-services.service";
+import { Author } from '../author';
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-all-authors',
@@ -8,9 +10,50 @@ import { AllServicesService } from "../services/all-services.service";
 })
 export class AllAuthorsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: AllServicesService, private fb: FormBuilder) { }
+
+  authors: Author[];
+  formAdd: FormGroup;
+
+  accesFormulaireAdd: boolean = false;
+  accesDetail: boolean = false;
 
   ngOnInit(): void {
+    this.service.getAll().subscribe(data => {this.authors = data; console.log(data)});
   }
 
+  changeAccesFormulaireAdd(): void {
+    this.accesFormulaireAdd = !this.accesFormulaireAdd;
+  }
+
+  changeAccesDetail(): void {
+    this.accesDetail = !this.accesDetail;
+  }
+
+  createFormAdd(): FormGroup {
+    return this.fb.group({
+      firstname: [''],
+      lastname: [''],
+      nationality: [''],
+      dob: [''],
+      dod: [''],
+      dead: Boolean
+    }) 
+  }
+  
+  afficheForm(): void {
+    this.accesFormulaireAdd = true;
+    this.formAdd = this.createFormAdd();
+  }
+
+  createAuthor() {
+    const newAuthor = this.formAdd.value;
+    if (!newAuthor.dod) {
+      newAuthor.dead = false;
+    } else {
+      newAuthor.dead = true;
+    }
+    this.changeAccesFormulaireAdd();
+    return this.service.create(newAuthor).subscribe(() => console.log("ok"));
+  }
 }
