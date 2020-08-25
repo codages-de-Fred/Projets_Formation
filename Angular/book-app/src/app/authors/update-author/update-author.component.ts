072b9fdf3cd53a497a6b199ee58dba1e39234ab7
fred80 <fred.nath.lui@live.fr>
2020-08-25 19:29:59 +0200

    update author + error 500

--- Angular/book-app/src/app/authors/update-author/update-author.component.ts
+++ Angular/book-app/src/app/authors/update-author/update-author.component.ts
@@ -13,38 +13,96 @@ export class UpdateAuthorComponent implements OnInit {
 
   constructor(private service: AllServicesService, private fb: FormBuilder, private route: ActivatedRoute, private router: Router) { }
 
+  dateDod: string;
   id: number;
   updateAuthor: Author;
   changeForm: FormGroup;
+  dateForm: FormGroup;
+  editDateForm: FormGroup;
+  changeDate: boolean = false;
+
+  firstname;
+  lastname;
+  nationality;
+  dob;
+  dod;
+  dead: boolean;
 
   ngOnInit(): void {
     this.route.params.subscribe(params => this.id = params.author);
     this.service.getOneAuthor(this.id).subscribe(
       data => {this.updateAuthor=data;
-      this.changeForm = this.createForm(this.updateAuthor);
+      this.changeForm = this.createChangeForm(this.updateAuthor);
+      this.dateForm = this.createDateForm(this.updateAuthor);
       });
   }
 
-  createForm(author: Author): FormGroup {
+  createChangeForm(author: Author): FormGroup {
+    console.log(author.dod)
     return this.fb.group({
       firstName: [author.firstName],
       lastName: [author.lastName],
       nationality: [author.nationality],
-      dob: [new Date(author.dob).toLocaleDateString()],
-      dod: [new Date(author.dod).toLocaleDateString()],
       dead: Boolean
     }) 
   }
+  
+  createDateForm(author: Author): FormGroup {
+    //gestion de la date de décès si existante
+    if(author.dod !== null) {
+      this.dateDod = new Date(author.dod).toLocaleDateString();
+    } else {
+      this.dateDod = "";
+    }
+    return this.fb.group({
+      dob: [new Date(author.dob).toLocaleDateString()],
+      dod: [this.dateDod],
+    })
+  }
+
+  changeDateForm() {
+    this.editDateForm = this.newDateForm();
+    this.changeDate = true;
+  }
+
+  newDateForm(): FormGroup {
+    return this.fb.group({
+      dob: [""],
+      dod: [""]
+    })
+  }
 
   update() {
-    const newAuthor = this.changeForm.value;
+    if (this.changeDate) {
+      this.dob = Object.values(this.editDateForm.value)[0];
+      this.dod = Object.values(this.editDateForm.value)[1];
+      console.log("change")
+    } else {
+      this.dob = this.updateAuthor.dob;
+      this.dod = this.updateAuthor.dod;
+      console.log("ok")
+    }
+    this.firstname = Object.values(this.changeForm.value)[0];
+    this.lastname = Object.values(this.changeForm.value)[1];
+    this.nationality = Object.values(this.changeForm.value)[2];
+    const newAuthor: Author = {
+      id: this.id,
+      firstName: this.firstname,
+      lastName: this.lastname,
+      nationality: this.nationality,
+      dob: this.dob,
+      dod: this.dod,
+      dead: this.dead,
+      books: this.updateAuthor.books
+    };
+    console.log(newAuthor)
     if (!newAuthor.dod) {
       newAuthor.dead = false;
       newAuthor.dod = null;
     } else {
       newAuthor.dead = true;
     }
-    return this.service.create(newAuthor).subscribe(() => 
+    return this.service.update(newAuthor).subscribe(() => 
       this.router.navigate(["/author"])
     );
   }
