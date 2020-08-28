3edd1f1e97fc50ff8d0a2c99e39c3f39cfc4d57b
fred80 <fred.nath.lui@live.fr>
2020-08-28 19:11:23 +0200

    update de categories

--- Angular/nodeApi/src/app/app-routing.module.ts
+++ Angular/nodeApi/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { ProductsComponent } from "./products/products/products.component";
 import { UpdateProductComponent } from "./products/update-product/update-product.component";
 
 import { CategoriesComponent } from "./categories/categories/categories.component";
+import { UpdateComponent } from "./categories/update/update.component";
 
 const routes: Routes = [
   {path: "product", component: ProductsComponent},
   {path: "update-product/:id", component: UpdateProductComponent},
-  {path: "categories", component: CategoriesComponent}
+  {path: "categories", component: CategoriesComponent},
+  {path: "update-categories/:id", component: UpdateComponent}
 ];
 
 @NgModule({
--- Angular/nodeApi/src/app/categories/update/update.component.ts
+++ Angular/nodeApi/src/app/categories/update/update.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ServiceService } from "../services/service.service";
+import { Categories } from '../categories';
 
 @Component({
   selector: 'app-update',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: ServiceService, private fb: FormBuilder, private route: ActivatedRoute, private router: Router) { }
+
+  category: Categories;
+  id: string;
+
+  updateForm: FormGroup;
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.id = params.id;
+      this.getOne(this.id);
+    })
+  }
+
+  getOne(id: string) {
+    this.service.getOne(id).subscribe((data) => {
+      this.category = data;
+      this.updateForm = this.createForm();
+    });
+  }
+
+  createForm(): FormGroup {
+    if (this.category) {
+      return this.fb.group({
+        name: [this.category.name],
+        description: [this.category.description],
+      });
+    }
+  }
+
+  update() {
+    const newCategory = this.updateForm.value;
+    return this.service.update(this.id, newCategory).subscribe(() => {
+      this.router.navigate(['/categories']);
+    });
   }
 
 }
