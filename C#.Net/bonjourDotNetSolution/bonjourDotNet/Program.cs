0e2e8d67c3e1a1e817cf75922a5d7d851ed7ef73
fred80 <fred.nath.lui@live.fr>
2020-08-30 17:02:36 +0200

    démarrage nv projet C#

---
+++ C#.Net/bonjourDotNetSolution/bonjourDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bonjourDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
